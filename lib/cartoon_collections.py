fd34e37a8796f4c6ed75501ca2fafee604fa04a1
Joy Olago <joydanielle62@gmail.com>
2023-09-12 21:19:19 +0300

    completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
 def long_planeteer_calls():
-    pass
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
 def find_the_cheese():
-    pass
+     cheeses = ["cheddar", "gouda", "camembert"]
+     for snack in snacks:
+        if snack in cheeses:
+            return snack
+     return None
